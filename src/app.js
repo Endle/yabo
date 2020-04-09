31799001a1215bedc6f24b67ee785fde7accf7cb
Zhenbo Li <litimetal@gmail.com>
2020-04-08 22:21:08 -0700

    Load local record

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
  /* eslint-disable */
+// security.fileuri.strict_origin_policy
+// No need to be true/false
 import Vue from 'vue'
 // import App from '@/components/App'
 
@@ -11,14 +13,49 @@ new Vue({ // eslint-disable-line no-new
 })
 
 new Vue({ // eslint-disable-line no-new
+  el: '#currentAccounts',
+  data: {
+    beancountFileList: 'Nothing yet'
+  }
+})
+
+//https://stackoverflow.com/a/52947649/1166518
+function splitLines(t) { return t.split(/\r\n|\r|\n/); }
+
+
+const processLocalBeancountRecordSingleLine = function(record) {
+    console.log(record);
+}
+
+const processLocalBeancountRecord = function(records) {
+    let lines = splitLines(records);
+    for (var i=0,len=records.length; i<len; i++) {
+        processLocalBeancountRecordSingleLine(lines[i]);
+    }
+}
+
+const fetchFileText = async file => {
+    let fr = new FileReader();
+    fr.onload=function() {
+        processLocalBeancountRecord(fr.result);
+    }
+    fr.readAsText(file);
+}
+
+new Vue({ // eslint-disable-line no-new
     methods: {
         loadBeancount(event) {
-            console.log(event.target.files);
+            var f;
+            for (var i=0, len=event.target.files.length; i<len; i++) {
+                let file = event.target.files[i];
+                console.log(file);
+                fetchFileText(file);
+            }
+            //console.log(event.target.files);
         },
     },
   el: '#loadDir',
   data: {
-    message: 'a Hello Vue!' + new Date().toLocaleString()
   }
 })
 
